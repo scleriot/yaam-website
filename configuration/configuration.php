41aa638cd22b06161890a4b7e25ede3025e7ecf1
Simon Cleriot <malgon33@gmail.com>
2011-07-02 13:44:45 +0200

    Forgottent a ";"

--- configuration/configuration.php
+++ configuration/configuration.php
@@ -5,6 +5,6 @@ include_once("credentials.php");
 $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 $bdd = new PDO('mysql:host='.$HOST.';dbname='.$DB, $USER, $PASSWORD, $pdo_options);
 
-$AAPT_DIR="/usr/bin/aapt"
+$AAPT_DIR="/usr/bin/aapt";
 
 ?>
